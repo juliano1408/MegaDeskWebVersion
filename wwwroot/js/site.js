51833e2eb98020ba141902110c510418f64e8edd
Dillon Godwin <ddgodwin01@byui.edu>
2022-02-26 15:17:22 -0700

    Dillons Part/ Front End

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function setImage(select) {
+    var image = document.getElementsByName("image-swap")[0];
+    image.src = select.options[select.selectedIndex].id;
+}
